53d9f774ba1f6ccbc94907f437ed9345a62ac1bf
Alysson Geremia <104856858+alyssongeremia@users.noreply.github.com>
2023-05-15 23:32:11 -0300

    update services

--- index.js
+++ index.js
@@ -13,3 +13,37 @@ btnClose.addEventListener("click", function() {
     btnHamburguer.classList.remove('hidden')
     menuMobile.id = "hidden"
 })
+
+// scrollbar
+const controls = document.querySelectorAll('.control')
+let currentItem = 0
+let itens = document.querySelectorAll('.item')
+const maxItens = itens.length
+
+controls.forEach(control => {
+    control.addEventListener('click', () => {
+        const isLeft = control.classList.contains("arrow-left")
+
+        if(isLeft) {
+            currentItem -= 1;
+        } else {
+            currentItem += 1;
+        }
+
+        if(currentItem >= maxItens) {
+            currentItem = 0;
+        }
+
+        if(currentItem < 0) {
+            currentItem = maxItens -1;
+        }
+
+        itens.forEach(item => item.classList.remove('current-item'))
+
+        itens[currentItem].scrollIntoView({
+            inline: "center", behavior: "smooth"
+        })
+
+        itens[currentItem].classList.add('current-item')
+    })
+})
